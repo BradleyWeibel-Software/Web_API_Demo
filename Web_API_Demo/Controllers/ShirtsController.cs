8bd7b45a51690b282f38d3d2e664a0ec0b5a07c3
BradleyWeibel-Software <downhillrid3r@gmail.com>
2026-03-14 23:22:07 +0100

    Comments explaining API URLs that reach controller methods

--- Web_API_Demo/Controllers/ShirtsController.cs
+++ Web_API_Demo/Controllers/ShirtsController.cs
@@ -6,30 +6,35 @@ namespace Web_API_Demo.Controllers
     [Route("api/[controller]")]
     public class ShirtsController : ControllerBase
     {
+        // GET: https://localhost:7104/api/shirts
         [HttpGet]
         public string GetShirts()
         {
             return "Reading all shirts";
         }
 
+        // GET : https://localhost:7104/api/shirts/1
         [HttpGet("{id}")]
         public string GetShirtById(int id)
         {
             return $"Reading shirt: {id}";
         }
 
+        // POST: https://localhost:7104/api/shirts
         [HttpPost]
         public string CreateShirt()
         {
             return $"Creating a shirt";
         }
 
+        // PUT: https://localhost:7104/api/shirts/1
         [HttpPut("{id}")]
         public string UpdateShirt(int id)
         {
             return $"Updating shirt: {id}";
         }
 
+        // DELETE: https://localhost:7104/api/shirts/1
         [HttpDelete("{id}")]
         public string DeleteShirt(int id)
         {
