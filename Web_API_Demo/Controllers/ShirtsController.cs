5a3d11cdd2ca1d4ab9fcf080b3c0b69849aab2d6
BradleyWeibel-Software <downhillrid3r@gmail.com>
2026-03-14 23:17:34 +0100

    API routing through controller and not minimalist API handling. Removal of pants controller

--- Web_API_Demo/Controllers/ShirtsController.cs
+++ Web_API_Demo/Controllers/ShirtsController.cs
@@ -3,28 +3,34 @@
 namespace Web_API_Demo.Controllers
 {
     [ApiController]
+    [Route("api/[controller]")]
     public class ShirtsController : ControllerBase
     {
+        [HttpGet]
         public string GetShirts()
         {
             return "Reading all shirts";
         }
 
+        [HttpGet("{id}")]
         public string GetShirtById(int id)
         {
             return $"Reading shirt: {id}";
         }
 
+        [HttpPost]
         public string CreateShirt()
         {
             return $"Creating a shirt";
         }
 
+        [HttpPut("{id}")]
         public string UpdateShirt(int id)
         {
             return $"Updating shirt: {id}";
         }
 
+        [HttpDelete("{id}")]
         public string DeleteShirt(int id)
         {
             return $"Deleting shirt: {id}";
--- Web_API_Demo/Program.cs
+++ Web_API_Demo/Program.cs
@@ -1,6 +1,9 @@
+using Microsoft.AspNetCore.Http.HttpResults;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddControllers();
 
 var app = builder.Build();
 
@@ -8,48 +11,6 @@ var app = builder.Build();
 
 app.UseHttpsRedirection();
 
-//app.UseRouting();
-
-//app.UseAuthentication();
-
-//app.UseAuthorization();
-
-// Routing
-
-// "/shirts"
-app.MapGet("/shirts", () =>
-{
-    return "Reading all the shirts";
-    //return new[]
-    //{
-    //    new { Id = 1, Name = "Red Shirt", Size = "M" },
-    //    new { Id = 2, Name = "Blue Shirt", Size = "L" },
-    //    new { Id = 3, Name = "Green Shirt", Size = "S" }
-    //};
-});
-
-app.MapGet("/shirts/{id}", (int id) =>
-{
-    return $"Reading shirt with ID: {id}";
-    //return new { Id = id, Name = $"Shirt {id}", Size = "M" };
-});
-
-app.MapPost("/shirts", () =>
-{
-    return $"Creating a shirt";
-    // In a real application, you would save the shirt to a database here
-});
-
-app.MapPut("/shirts/{id}", (int id) =>
-{
-    return $"Updating shirt with ID: {id}";
-    // In a real application, you would update the shirt in the database here
-});
-
-app.MapDelete("/shirts/{id}", (int id) =>
-{
-    return $"Deleting shirt with ID: {id}";
-    // In a real application, you would delete the shirt from the database here
-});
+app.MapControllers();
 
 app.Run();
