f40bc172bcabfb55f61727611535c2d80c15b891
BradleyWeibel-Software <downhillrid3r@gmail.com>
2026-03-16 22:02:18 +0100

    Passing data in different places. Regions in Controller. Shirts model added.

--- Web_API_Demo/Controllers/ShirtsController.cs
+++ Web_API_Demo/Controllers/ShirtsController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Web_API_Demo.Model;
 
 namespace Web_API_Demo.Controllers
 {
@@ -6,6 +7,8 @@ namespace Web_API_Demo.Controllers
     [Route("api/[controller]")]
     public class ShirtsController : ControllerBase
     {
+        #region GET
+
         // GET: https://localhost:7104/api/shirts
         [HttpGet]
         public string GetShirts()
@@ -20,12 +23,52 @@ namespace Web_API_Demo.Controllers
             return $"Reading shirt: {id}";
         }
 
+        // GET : https://localhost:7104/api/shirts/1/blue
+        [HttpGet("{id}/{colour}")]
+        public string GetShirtByIdAndColour(int id, [FromRoute]string colour)
+        {
+            return $"Reading shirt: {id} with colour: '{colour}'";
+        }
+
+        // GET : https://localhost:7104/api/shirts/getshirt/1?colour=blue&size=large
+        [HttpGet("getshirt/{id}")]
+        public string GetShirtByIdAndPassInfo(int id, [FromQuery]string colour, [FromQuery]string size)
+        {
+            return $"Reading shirt: {id}, with size '{size}' and colour: '{colour}'";
+        }
+
+        // GET : https://localhost:7104/api/shirts/getshirtpassheader/1?colour=blue
+        [HttpGet("getshirtpassheader/{id}")]
+        public string GetShirtByIdAndHeaderInfo(int id, [FromQuery] string colour, [FromHeader] string size)
+        {
+            return $"Reading shirt: {id}, with size '{size}' and colour: '{colour}'";
+        }
+
+        #endregion
+
+        #region POST
+
         // POST: https://localhost:7104/api/shirts
         [HttpPost]
         public string CreateShirt()
         {
-            return $"Creating a shirt";
+            return $"Creating a shirt from bupkis I guess";
         }
+
+        // POST: https://localhost:7104/api/shirts/createshirtwithbody
+        [HttpPost("createshirtwithbody")]
+        public string CreateShirtWithBody([FromBody]Shirt shirtData)
+        {
+            return $"Creating a shirt with \n" +
+                $"id: {shirtData.Id}, \n" +
+                $"brand: '{shirtData.Brand}', \n" +
+                $"size: {shirtData.Size}, \n" +
+                $"colour: {shirtData.Colour}";
+        }
+
+        #endregion
+
+        #region PUT, Patch
 
         // PUT: https://localhost:7104/api/shirts/1
         [HttpPut("{id}")]
@@ -34,11 +77,17 @@ namespace Web_API_Demo.Controllers
             return $"Updating shirt: {id}";
         }
 
+        #endregion
+
+        #region DELETE
+
         // DELETE: https://localhost:7104/api/shirts/1
         [HttpDelete("{id}")]
         public string DeleteShirt(int id)
         {
             return $"Deleting shirt: {id}";
         }
+
+        #endregion
     }
 }
---
+++ Web_API_Demo/Model/Shirt.cs
@@ -0,0 +1,11 @@
+﻿namespace Web_API_Demo.Model
+{
+    public class Shirt
+    {
+        public int Id { get; set; }
+        public string? Brand { get; set; }
+        public string? Colour { get; set; }
+        public int Size { get; set; }
+        public string? Sex { get; set; }
+    }
+}
