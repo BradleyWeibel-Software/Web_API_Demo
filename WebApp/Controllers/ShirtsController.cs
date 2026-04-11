2619fccce1220ed1a9622712a113b1c7afd544f5
BradleyWeibel-Software <downhillrid3r@gmail.com>
2026-04-12 00:14:18 +0200

    Frontend improvement through MVC .NET CORE. New project added to solution for web app frontend

---
+++ WebApp/Controllers/ShirtsController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Web_API_Demo.Data;
+using Web_API_Demo.Repositories;
+
+namespace WebApp.Controllers
+{
+    public class ShirtsController : Controller
+    {
+        private readonly ShirtRepository shirtRepository;
+
+        public ShirtsController(ApplicationDBContext dbContext)
+        {
+            shirtRepository = new ShirtRepository(dbContext);
+        }
+
+        public IActionResult Index()
+        {
+            var shirts = shirtRepository.GetShirts();
+            return View(shirts);
+        }
+    }
+}
