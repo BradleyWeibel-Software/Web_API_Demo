930b52ab88259581c3d8c49341e1828d9506518d
BradleyWeibel-Software <downhillrid3r@gmail.com>
2026-04-12 22:32:01 +0200

    Update shirt UI functionality

--- WebApp/Controllers/ShirtsController.cs
+++ WebApp/Controllers/ShirtsController.cs
@@ -13,10 +13,16 @@ namespace WebApp.Controllers
             this.webApiExecuter = webApiExecuter;
         }
 
+        #region Show all shirts
+
         public async Task<IActionResult> Index()
         {
             return View(await webApiExecuter.InvokeGet<List<Shirt>>("shirts"));
         }
+
+        #endregion
+
+        #region Create Shirt
 
         public IActionResult CreateShirt()
         {
@@ -26,15 +32,40 @@ namespace WebApp.Controllers
         [HttpPost]
         public async Task<IActionResult> CreateShirt(Shirt shirt)
         {
-            //if (ModelState.IsValid())
+            //if (ModelState.IsValid()) // TODO: Add validation
             //{
-                var response = await webApiExecuter.InvokePost<Shirt>("shirts/createshirtwithbody", shirt);
-                if (response != null)
-                {
-                    return RedirectToAction(nameof(Index));
-                }
+            var response = await webApiExecuter.InvokePost<Shirt>("shirts/createshirtwithbody", shirt);
+            if (response != null)
+                return RedirectToAction(nameof(Index));
             //}
             return View(shirt);
         }
+
+        #endregion
+
+        #region Edit Shirt
+
+        public async Task<IActionResult> EditShirt(int shirtId)
+        {
+            var shirt = await webApiExecuter.InvokeGet<Shirt>($"shirts/{shirtId}");
+
+            if (shirt != null)
+                return View(shirt);
+
+            return NotFound();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> EditShirt(Shirt shirt)
+        {
+            //if (ModelState.IsValid()) // TODO: Add validation
+            //{
+            await webApiExecuter.InvokePut<Shirt>($"shirts", shirt);
+                return RedirectToAction(nameof(Index));
+            //}
+            // return View(shirt);
+        }
+
+        #endregion
     }
 }
--- WebApp/Data/WebAPIExecuter.cs
+++ WebApp/Data/WebAPIExecuter.cs
@@ -25,5 +25,12 @@ namespace WebApp.Data
 
             return await response.Content.ReadFromJsonAsync<T>();
         }
+
+        public async Task InvokePut<T>(string relativeUrl, T obj)
+        {
+            var httpClient = httpClientFactory.CreateClient(apiName);
+            var response = await httpClient.PutAsJsonAsync(relativeUrl, obj);
+            response.EnsureSuccessStatusCode();
+        }
     }
 }
